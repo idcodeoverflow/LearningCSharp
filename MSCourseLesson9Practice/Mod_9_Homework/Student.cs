0a35af6dd2a505e67a87d3e7e2d55b1d7a61b5cc
David Garcia <david.programming@gmail.com>
2016-03-23 16:46:43 -0600

    Add practice 9 files

---
+++ MSCourseLesson9Practice/Mod_9_Homework/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    class Student
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String City { get; set; }
+
+        public Student()
+        {
+            this.FirstName = "";
+            this.LastName = "";
+            this.City = "";
+        }
+
+        public Student(String FirstName, String LastName, String City)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.City = City;
+        }
+    }
+}
