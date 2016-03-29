d51fc95151e79dbd135ef65aaa23c9bb25f4dcda
David Garcia <david.programming@gmail.com>
2016-03-28 21:45:36 -0600

    Add practices 1,2,3

---
+++ MSCourseTask2Console/MSCourseTask2Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSCourseTask2Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsNumber = 8;
+            int columnsNumber = 8;
+            bool columnPair;
+            
+            for(int row = 0; row < rowsNumber; row++)
+            {
+                string toPrint = "";
+                for(int column = 0; column < columnsNumber; column++)
+                {
+                    columnPair = Convert.ToBoolean(column % 2);
+                    if(row % 2 != 0)
+                    {
+                        if (columnPair)
+                        {
+                            toPrint += "X";
+                        }
+                        else
+                        {
+                            toPrint += "O";
+                        }
+                    }
+                    else
+                    {
+                        if (columnPair)
+                        {
+                            toPrint += "O";
+                        }
+                        else
+                        {
+                            toPrint += "X";
+                        }
+                    }
+                }
+                Console.WriteLine(toPrint);
+            }
+        }
+    }
+}
